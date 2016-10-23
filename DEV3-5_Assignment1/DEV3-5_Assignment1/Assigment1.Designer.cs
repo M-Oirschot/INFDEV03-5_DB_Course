431d1884bfadedb36bb287b84bd48ce7ece9a02b
Marco van Oirschot <marcovanoirschot@hotmail.com>
2016-10-23 16:26:03 +0200

    Connect with database and insert new rows

--- DEV3-5_Assignment1/DEV3-5_Assignment1/Assigment1.Designer.cs
+++ DEV3-5_Assignment1/DEV3-5_Assignment1/Assigment1.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBsn = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBsn
+            // 
+            this.txtBsn.Location = new System.Drawing.Point(134, 53);
+            this.txtBsn.Name = "txtBsn";
+            this.txtBsn.Size = new System.Drawing.Size(100, 20);
+            this.txtBsn.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(403, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Assigment1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(471, 326);
-            this.Name = "Form1";
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtBsn);
+            this.Controls.Add(this.button1);
+            this.Name = "Assigment1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBsn;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- DEV3-5_Assignment1/DEV3-5_Assignment1/Assigment1.cs
+++ DEV3-5_Assignment1/DEV3-5_Assignment1/Assigment1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DEV3_5_Assignment1
 {
@@ -15,6 +16,65 @@ namespace DEV3_5_Assignment1
         public Assigment1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = "Server=localhost;Database=dev3-05;Uid=root;Pwd=";
+            MySqlConnection connection = new MySqlConnection(ConnectionString); ;
+            MySqlCommand cmd;
+            connection.Open();
+
+            try
+            {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO employee(bsn)VALUES(@bsn)";
+                cmd.Parameters.AddWithValue("@bsn", int.Parse(txtBsn.Text));
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if(connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    LoadData();
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            string ConnectionString = "Server=localhost;Database=dev3-05;Uid=root;Pwd=";
+            MySqlConnection connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+            try
+            {
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM employee";
+                MySqlDataAdapter adapt = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if(connection.State == ConnectionState.Open)
+                {
+                    connection.Clone();
+                }
+            }
+
         }
     }
 }
